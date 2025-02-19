e4dbbee9f432fc451052796c0beb406c4efb6b9e
Rajeev <rajeevkumarjbo2003@gmail.com>
2025-02-19 10:27:16 +0530

    added error middleware + schema&models

--- app.js
+++ app.js
@@ -6,10 +6,13 @@ import userRouter from './routes/user.routes.js';
 import subscriptionRouter from './routes/subscription.routes.js';
 import authRouter from './routes/auth.routes.js';
 import connectToDb from './database/mongodb.js';
+import cookieParser from 'cookie-parser';
 
 const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
 
 app.use('/api/v1/users',userRouter);
 app.use('/api/v1/auth',authRouter);
